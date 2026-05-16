8510c23b209e7bb197f9c51496a15069108cbed9
sintaxisBywen <wenfdez26@gmail.com>
2026-05-16 16:55:07 -0600

    Programa que resta dos números

--- AdiosJava.java
+++ AdiosJava.java
@@ -1,6 +1,4 @@
-
 import java.util.Scanner;
-
 public class AdiosJava{
     public static void main(String[] args) {
       int numero1, numero2;
@@ -11,6 +9,17 @@ public class AdiosJava{
       numero2 = scanner.nextInt();
       int resultado = numero1 + numero2;
       System.out.println("La suma es: " + resultado );
+      System.out.println("=================================");
+
+
+      int resta = restar(numero1, numero2);
+      System.out.println("La resta es: " + resta);
 
     }
+
+    public static int restar (int n1,  int n2){
+
+        return n1 - n2;
+    }
+    
 }
